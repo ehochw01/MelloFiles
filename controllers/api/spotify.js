dba05475e835a96c9257579a954020dc4a71daaf
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-11-28 14:50:36 -0500

    tweaked the search algorithm

--- controllers/api/spotify.js
+++ controllers/api/spotify.js
@@ -12,22 +12,33 @@ router.get('/search/:artist', spotifyAuth, async (req, res) => {
     });
      // Save the access token so that it's used in future calls
     spotifyApi.setAccessToken(req.session.spotify_token);
-    const searchString = req.params.artist;
+    const searchString = req.params.artist.toLowerCase();
     // hard coded to pick most popular result
     const searchArtistData = await spotifyApi.searchArtists(searchString, {limit: 10});
     const artists = searchArtistData.body.artists.items;
-    // console.log("artists", artists);
-    // console.log("Does the name match?", artists[1].name == searchString);
-    var artistId = artists[0].id;
-    // if there is a tie for popularity
-    if (artists[0].popularity == artists[1].popularity) {
+    console.log("artists", artists);
+    console.log("Does artists[0] name match?", artists[0].name.toLowerCase() == searchString);
+    var artistId = undefined;
+    if (artists[0].name.toLowerCase() == searchString) {
+      artistId = artists[0].id;
+    } else {
+      let matched = false;
+      let popularity = 0;
       for (let i=0; i < artists.length; i++) {
-        if (artists[i].name == searchString) {
-          artistId = artists[i].id;
+        if (artists[i].name.toLowerCase() == searchString) {
+          let artist = artists[i];
+          // if more than one name match, take the more popular artist
+          if (!matched || (matched && artist.popularity > popularity)) {
+            artistId = artist.id;
+            popularity = artist.popularity;
+          }
+          matched = true;
         }
       }
-
-    } 
+    }
+    if (artistId === undefined) {
+      artistId = artists[0].id;
+    }
     res.status(200).json(artistId);
   } catch (err) {
     console.log(err);
